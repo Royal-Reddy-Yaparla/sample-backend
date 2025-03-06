01f7b081c25a38daab0ae1560f1a908a9b9a0d7f
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2025-03-06 11:30:34 +0530

    backend

---
+++ src/main/java/fusionIQ/AI/V2/fusionIq/service/DataSourceConfig.java
@@ -0,0 +1,34 @@
+package fusionIQ.AI.V2.fusionIq.service;
+
+import com.zaxxer.hikari.HikariConfig;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://ec2-47-129-178-113.ap-southeast-1.compute.amazonaws.com:3306/prod?createDatabaseIfNotExist=true");
+        config.setUsername("admin");
+        config.setPassword("Admin@123");
+        config.addDataSourceProperty("connectionInitSql", "SET GLOBAL max_allowed_packet=134217728"); // 128 MB
+        return new HikariDataSource(config);
+    }
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+}
+
